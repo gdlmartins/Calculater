b51e306cbf0556028ea1969f916469d4ff4bc7de
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-23 16:45:53 +0800

    solving partily . problem

--- index.js
+++ index.js
@@ -1,92 +1,38 @@
+const display = document.querySelector(".display");
+let calculo = [];
+let conta = 1;
+let entradaClick = [];
+display.value = 0;
 
-
-const display = document.querySelector('.display');
-let calculo=[];
-let conta=1;
-let entradaClick=[];
-display.value=0;
-
-const enteringAKey =(e)=>{ 
-  entradaClick = [...entradaClick, display.value = e.target.innerText];
-  display.value = entradaClick =entradaClick.join('');
-};
-
-const operator = operador => {
-  entradaClick = [...entradaClick, operador ]
-  display.value = entradaClick =entradaClick.join('');
-} ; 
-
-const resultFunction = () => { 
-  entradaClick =  [eval(entradaClick)]
- display.value = entradaClick;
-}
-
-const operatorSpeciais =operadorSpecial => { 
-  if(entradaClick.length >0  && entradaClick){ 
-    console.log("entrou click")
-    entradaClick = [...entradaClick, operadorSpecial ].join('')
-    entradaClick= [eval(entradaClick)]
-    console.log(operadorSpecial)
-    console.log(entradaClick)
-    display.value =entradaClick
-  }else {
-    console.log("nao entrou click")
-    display.value= "Error NAN"
+const enteringAKey = (e) => {
+  const input = e.target.innerText;
+  if (input === ".") {
+    if (entradaClick.find((item) => item === ".")) {
+      input = "";
+    }
   }
 
-}
-
-const reset = () =>  {
+  entradaClick = [...entradaClick, (display.value = input)];
+  display.value = entradaClick = entradaClick.join("");
+};
+const operator = (operador) => {
+  entradaClick = [...entradaClick, operador];
+  display.value = entradaClick = entradaClick.join("");
+};
+const resultFunction = () => {
+  entradaClick = [eval(entradaClick)];
+  display.value = entradaClick;
+};
+const operatorSpeciais = (operadorSpecial) => {
+  if (entradaClick.length > 0) {
+    entradaClick = [...entradaClick, operadorSpecial].join("");
+    entradaClick = [eval(entradaClick)];
+    display.value = entradaClick;
+  } else {
+    display.value = "Error NAN";
+  }
+};
+const reset = () => {
   entradaClick = [];
-  display.value=0; 
-}
-
-
-    // calculo = [...calculo , entradaClick ];
-    // entradaClick = [];
-    // // console.log( [...calculo, operador] );
-    // // display.value = eval([...calculo].join(''));
-    // // calculo = eval([...calculo].join(''));
-    // return calculo = [...calculo, operador] ;
-    
-    // //    return calculo = [...calculo, operador]
-
-
-//     calculo = [...calculo , entradaClick ];
-//     entradaClick = [];
-//     console.log('32', calculo);
-//     console.log('calculo spread operator',...calculo)
-//     calculo = [...calculo].join('')
-//     console.log('36', calculo);
-//     calculo = [eval(calculo)];
-//     display.value =calculo;
-    // console.log(calculo);
-//     return calculo =[] ;
-
-
-
-
-//   calculo = [...calculo , entradaClick ];
-//   entradaClick = [];
-//   console.log('49', [...calculo, operadorSpecial] );
-//   calculo = [...calculo, operadorSpecial].join('');
-//   calculo = eval(calculo);
-//   display.value = calculo;
-//   return calculo = [calculo] ;
-
-
-
-
-
-
-
-//   display.value = calculo =[];
-
-
-
-// if(entradaClick === '±'){ conta *=-1 }
-// if(entradaClick === '÷'){ entradaClick = '/' }
-// console.log(" Line13 entering", entradaClick);
-// console.log(" Line13 entering", conta);
-
-
+  display.value = 0;
+};
